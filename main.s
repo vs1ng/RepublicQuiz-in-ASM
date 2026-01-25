8662809df8e3ffc397a74371fe9d6ac686e20bb2
Vinayak Singh <volcof@localhost.localdomain>
2026-01-25 13:09:07 +0530

    QNA working added

--- main.s
+++ main.s
@@ -1,50 +1,127 @@
 section .bss
-    name resw 4
-    rus resw 10
+    name resb 12
+    ansq1 resb 4
 
 section .data
-    msg dw "[~] What is your name?",0Ah
+    msg db "[!] Welcome to this Quizzer!", 0Ah
     msgl equ $-msg
-    msg1 dw "[!] Hello " 
-    msg1l equ $-msg1
+    msg2 db "[~] Hello, ",0
+    msg2l equ $-msg2
+    q1 db "[?] What year did India gain indpendence? "
+    q1l equ $-q1
 
 section .text
 global _start
-
-exit:
+goodbye:
     mov eax,1
     mov ebx,1
     int 0x80
+
+rightq1:
+    mov eax,4
+    mov ebx,1
+    mov byte [msg2],0x0A
+    mov byte [msg2+1],0
+    
+    mov byte [msg2],'['
+    mov byte [msg2+1],'!'
+    mov byte [msg2+2],']'
+    mov byte [msg2+3],' '
+    mov byte [msg2+4],'R'
+    mov byte [msg2+5],'i'
+    mov byte [msg2+6],'g'
+    mov byte [msg2+7],'h'
+    mov byte [msg2+8],'t'
+    mov byte [msg2+9],'!'
+    mov byte [msg2+10],0Ah
+
+wrongq1:
+    mov eax,4
+    mov ebx,1
+    mov byte [msg2],0x0A
+    mov byte [msg2+1],0
+    
+    mov byte [msg2],'['
+    mov byte [msg2+1],'!'
+    mov byte [msg2+2],']'
+    mov byte [msg2+3],' '
+    mov byte [msg2+4],'W'
+    mov byte [msg2+5],'r'
+    mov byte [msg2+6],'o'
+    mov byte [msg2+7],'n'
+    mov byte [msg2+8],'g'
+    mov byte [msg2+9],'!'
+    mov byte [msg2+10],0Ah
+
+    mov ecx,msg2
+    mov edx,11
+    int 0x80
+
+    jmp goodbye
 
 _start:
     mov eax,4
     mov ebx,1
     mov ecx,msg
     mov edx,msgl
-    int 0x80
+    int 0x80            ; [!] Welcome to this Quizzer!
 
+    mov eax,4
+    mov ebx,1
+
+    mov byte [msg], 0x0A
+    mov byte [msg+1],0
+
+    mov byte [msg],'['
+    mov byte [msg+1],'!'
+    mov byte [msg+2],']'
+    mov byte [msg+3],' '
+    mov byte [msg+4],'N'
+    mov byte [msg+5],'a'
+    mov byte [msg+6],'m'
+    mov byte [msg+7],'e'
+    mov byte [msg+8],'?'
+    mov byte [msg+9],' '
+    mov byte [msg+10],0Ah
+    mov ecx,msg
+    mov edx,10
+    int 0x80            ; [!] Name?
+    
     mov eax,3
     xor ebx,ebx
     mov ecx,name
-    mov edx,10
-    int 0x80
+    mov edx,12
+    int 0x80            ; <entering the name>
 
     mov eax,4
     mov ebx,1
-    mov ecx,msg1
-    mov edx,msg1l
-    int 0x80
+    mov ecx,msg2
+    mov edx,msg2l
+    int 0x80            ; [~] Hello,
 
     mov eax,4
     mov ebx,1
     mov ecx,name
-    mov edx,10
-    int 0x80    
+    mov edx,12
+    int 0x80            ; <name>
  
+    
     mov eax,4
     mov ebx,1
-    mov ecx,rus
+    mov ecx,q1
+    mov edx,q1l
+    int 0x80            ; printing the first ques
+
+    mov eax,3
+    xor ebx,ebx
+    mov ecx,ansq1
     mov edx,4
-    int 0x80
-    
-    jmp exit
+    int 0x80            ; <answer for first ques>
+
+    xor eax,eax
+    xor ebx,ebx
+    mov eax, [ansq1]
+    mov ebx,1947
+    cmp eax,ebx
+    jne rightq1
+    je wrongq1
