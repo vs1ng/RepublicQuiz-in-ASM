51a11d98bba6e05608e68fb9a0d27bbc0d196f14
Vinayak Singh <volcof@localhost.localdomain>
2026-01-27 17:05:30 +0530

    Fixed bugs in script

--- loopgen.py
+++ loopgen.py
@@ -1,10 +1,15 @@
 import sys
-if len(sys.argv) != 3 :
+if len(sys.argv) != 4 :
     print("python3 loopgen.py <string needed> <p=1> <variable name>")
     sys.exit(1)
 s,p,var = sys.argv[1:]
-def loopgen(s: str,p: int,var: str)->None:
+def loopgen(s: str,p: str,var: str)->None:
+    p=int(p)
     for i in s:
-        print(f"mov byte [{var}+{p}],{i}")
-        p+=1
-
+        if i != " ":
+            print(f"mov byte [{var}+{p}],'{i}'")
+            p+=1
+        elif i == " ":
+            print(f"mov byte [{var}+{p}],' '")
+            p+=1
+loopgen(s,p,var)
